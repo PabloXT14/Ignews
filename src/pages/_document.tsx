95a5de6ab4b0dbc1c20564c1befd342b0a1f3eed
Pablo Alan <pabloxt14@gmail.com>
2022-03-30 18:11:29 -0300

    feat: Add dynamic title and global style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-                    {/* <title> agora deve ser definido nas outras paginas ou no _app.tsx através do Componented <Head> <title>dsgsd</title> </Head> */}
-                    {/* <title>ig.news</title> */}
                 </Head>
                 <body>
                     <Main />
